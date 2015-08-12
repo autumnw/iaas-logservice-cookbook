878c44ada8dd89ac9c810b0913210ed842f98560
Autumn Wang <autumn.wang@autodesk.com>
2015-08-12 09:54:32 -0700

    Replace host with logsource, which is the real source hostname of the rsyslog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures iaas-logservice'
 long_description 'Installs/Configures iaas-logservice'
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'elasticsearch'
 depends 'apache_zookeeper'
