ad6b5ce1b0479a6f5b69bccded411051bc59df43
Autumn Wang <autumn.wang@autodesk.com>
2015-08-12 21:10:01 -0700

    Add sensulog

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures iaas-logservice'
 long_description 'Installs/Configures iaas-logservice'
-version          '0.1.6'
+version          '0.1.7'
 
 depends 'elasticsearch'
 depends 'apache_zookeeper'
